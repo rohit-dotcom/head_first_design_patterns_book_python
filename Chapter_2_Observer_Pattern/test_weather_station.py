4bb57bda0cc674125a6f3174f9e4161d66c2a387
Rohit Sharma <rohit.enggboy@gmail.com>
2026-08-30 15:54:12 +0530

    for adding forecast we had to modifiy the update function to make it more decoupled and not have dependencies at the level of observer

--- Chapter_2_Observer_Pattern/test_weather_station.py
+++ Chapter_2_Observer_Pattern/test_weather_station.py
@@ -1,5 +1,6 @@
-from weather import Station,WeatherData,CurrentDisplayObserver
+from weather import Station,WeatherData,CurrentDisplayObserver,ForecastDisplayObserver
 import numpy as np
+import random
 
 class NewStation(Station):
 
@@ -9,6 +10,10 @@ class NewStation(Station):
             return np.random.randint(100,200)
     def get_humidity(self):
             return np.random.randint(80,90)
+    def get_forecast(self):
+        first_parts=['Look out for','More of','Here Comes',]
+        second_parts=['Cold','Rains','Heat']
+        return f'{random.choice(first_parts)} {random.choice(second_parts)}!'
 
 def test_station_is_able_to_update_weatherdata_and_observer():
     new_station=NewStation()
@@ -44,3 +49,30 @@ def test_when_observer_unsubscribes_it_is_no_loger_able_to_get_updates():
     assert new_observer not in new_weather_data.observers
     new_weather_data.weathersChanged()
     assert new_observer.temp!=new_weather_data.temp
+
+def test_if_forecast_display_is_able_to_fetch_data_from_subject_upon_subscription():
+    new_station=NewStation()
+    new_weather_data=WeatherData(new_station)
+    forecast_observer=ForecastDisplayObserver(new_weather_data)
+
+    forecast_observer.subscribe()
+
+    new_weather_data.weathersChanged()
+
+    assert forecast_observer.forecast==new_weather_data.forecast
+
+
+def test_if_forecast_display_is_able_to_unsubscirbe():
+    new_station=NewStation()
+    new_weather_data=WeatherData(new_station)
+    forecast_observer=ForecastDisplayObserver(new_weather_data)
+
+    forecast_observer.subscribe()
+
+    new_weather_data.weathersChanged()
+
+    assert forecast_observer.forecast==new_weather_data.forecast
+    forecast_observer.unsubscribe()
+    assert forecast_observer not in new_weather_data.observers
+    new_weather_data.weathersChanged()
+    assert forecast_observer.forecast!=new_weather_data.forecast
