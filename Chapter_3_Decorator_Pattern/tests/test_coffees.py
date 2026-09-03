5e7278341db4daa710684d32528ab385e5e728f8
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-03 16:28:04 +0530

    added teh class instance variables

--- Chapter_3_Decorator_Pattern/tests/test_coffees.py
+++ Chapter_3_Decorator_Pattern/tests/test_coffees.py
@@ -8,32 +8,32 @@ def test_subclasses_able_to_instantiate():
 
 def test_decorator_able_to_use_superclass_and_subclasses():
     new_expresso=Expresso()
-    expresso_with_whip=Whip(new_expresso)
+    new_expresso=Whip(new_expresso)
 
-    assert expresso_with_whip.get_description()=='Expresso, whip'
-    assert expresso_with_whip.cost()==2.09
+    assert new_expresso.get_description()=='Expresso, whip'
+    assert new_expresso.cost()==2.09
 
 def test_house_blend_with_soy():
     hb=House_Blend()
-    hb_with_whip=Soy(hb)
+    hb=Soy(hb)
 
-    assert hb_with_whip.get_description()=='House Blend, soy'
-    assert hb_with_whip.cost()==1.04
+    assert hb.get_description()=='House Blend, soy'
+    assert hb.cost()==1.04
 
 
 def test_able_to_wrap_2_condiments_to_one_coffee():
     house_blend=House_Blend()
-    house_blend_with_whip=Whip(house_blend)
-    house_blend_with_whip_and_soy=Soy(house_blend_with_whip)
+    house_blend=Whip(house_blend)
+    house_blend=Soy(house_blend)
 
-    assert house_blend_with_whip_and_soy.get_description()=='House Blend, whip, soy'
-    assert house_blend_with_whip_and_soy.cost()==1.14
+    assert house_blend.get_description()=='House Blend, whip, soy'
+    assert house_blend.cost()==1.14
 
 
 def test_expresso_with_double_whip():
     ex=Expresso()
-    ex_wp=Whip(ex)
-    ex_wp_wp=Whip(ex_wp)
+    ex=Whip(ex)
+    ex=Whip(ex)
 
-    assert ex_wp_wp.get_description()=='Expresso, whip, whip'
-    assert ex_wp_wp.cost()==2.19
+    assert ex.get_description()=='Expresso, whip, whip'
+    assert ex.cost()==2.19
