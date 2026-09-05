8d7c021c02d6dca146ed39a241da699c4842be95
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-05 12:20:56 +0530

    added chapter 4 initial code

---
+++ chapter_4_Factory_Pattern/main.py
@@ -0,0 +1,5 @@
+from pizzas import orderPizza
+
+if __name__=="__main__":
+    # orderPizza('cheese')
+    orderPizza('pepperoni')
---
+++ chapter_4_Factory_Pattern/pizzas.py
@@ -0,0 +1,79 @@
+from abc import ABC,abstractmethod
+
+class Pizza(ABC):
+
+    @abstractmethod
+    def bake(self):
+        pass
+
+    @abstractmethod
+    def cut(self):
+        pass
+
+    @abstractmethod
+    def box(self):
+        pass
+
+class CheesePizza(Pizza):
+
+    def bake(self):
+        return "Baking cheese pizza!"
+
+    def cut(self):
+        return "Cutting in square pieces"
+
+    def box(self):
+        return "Boxing cheese pizza"
+    
+
+class PepperoniPizza(Pizza):
+
+    def bake(self):
+        return "Baking pepperoni pizza!"
+
+    def cut(self):
+        return "Cutting in diagonal pieces"
+
+    def box(self):
+        return "Boxing pepperoni pizza"
+
+
+class veggiePizza(Pizza):
+
+    def bake(self):
+        return "Baking veggie pizza!"
+
+    def cut(self):
+        return "Cutting in triangular pieces"
+
+    def box(self):
+        return "Boxing veggie pizza"
+
+class simplePizzaFactory():
+    pizza:Pizza=None
+
+    def get_pizza(self,type):
+        if type=="cheese":
+            pizza= CheesePizza()
+            
+        if type=="pepperoni":
+            pizza= PepperoniPizza()
+                
+        if type=="veggie":
+            pizza= veggiePizza()
+        return pizza
+
+class PizzaStore():
+    def __init__(self,factory:simplePizzaFactory):
+        self.factory=factory
+    
+    def orderPizza(self,type)->Pizza:
+        pizza:Pizza
+
+        pizza=self.factory.get_pizza(type)
+
+        pizza.bake()
+        pizza.cut()
+        pizza.box()
+        return pizza
+
