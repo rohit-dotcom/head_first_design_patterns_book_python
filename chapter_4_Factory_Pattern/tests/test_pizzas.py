e9b91909c8644860c6d34f26ec7eab5a39e03968
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-08 18:32:05 +0530

    corrected the abstract factory

--- chapter_4_Factory_Pattern/tests/test_pizzas.py
+++ chapter_4_Factory_Pattern/tests/test_pizzas.py
@@ -1,29 +1,39 @@
-from pizzas import Pizza,PepperoniPizza,CheesePizza,veggiePizza,PizzaStore,NYPizzaStore,ChicagoPizzaStore,NY_ingredients_factory
+from abstract_factory import Pizza,NYPizzaStore,ChicagoPizzaStore
 
-def test_able_to_create_instances_of_differen_pizzas():
+# def test_able_to_create_instances_of_differen_pizzas():
 
-    cheese_pizza=CheesePizza()
-    pep_piz=PepperoniPizza()
-    veg_piz=veggiePizza()
+#     cheese_pizza=CheesePizza()
+#     pep_piz=PepperoniPizza()
+#     veg_piz=veggiePizza()
 
-    assert cheese_pizza.bake()=='Baking cheese pizza!'
-    assert pep_piz.cut()=='Cutting in diagonal pieces'
-    assert veg_piz.box()=='Boxing veggie pizza'
+#     assert cheese_pizza.bake()=='Baking cheese pizza!'
+#     assert pep_piz.cut()=='Cutting in diagonal pieces'
+#     assert veg_piz.box()=='Boxing veggie pizza'
 
-def test_order_pizza_is_able_return_pizza_from_NY_Pizza_store():
-    pizza_store=NYPizzaStore()
-    pizza=pizza_store.orderPizza('veggie')
-    assert pizza.box()=='Boxing NYveggie pizza'
+# def test_order_pizza_is_able_return_pizza_from_NY_Pizza_store():
+#     pizza_store=NYPizzaStore()
+#     pizza=pizza_store.orderPizza('veggie')
+#     assert pizza.box()=='Boxing NYveggie pizza'
 
-def test_order_pizza_is_able_return_pizza_from_chicago_Pizza_store():
-    pizza_store=ChicagoPizzaStore()
-    pizza=pizza_store.orderPizza('veggie')
-    assert pizza.box()=='Boxing Chichago veggie pizza'
+# def test_order_pizza_is_able_return_pizza_from_chicago_Pizza_store():
+#     pizza_store=ChicagoPizzaStore()
+#     pizza=pizza_store.orderPizza('veggie')
+#     assert pizza.box()=='Boxing Chichago veggie pizza'
 
-def test_if_ingredient_factory_is_able_to_pass_on_ingredients_to_store():
+# def test_if_ingredient_factory_is_able_to_pass_on_ingredients_to_store():
 
-    ingredient_factory=NY_ingredients_factory()
-    pizza_store=NYPizzaStore(ingredient_factory)
-    pizza=pizza_store.orderPizza('veggie')
-    assert pizza.box()=='Boxing BYveggie pizza'
+#     ingredient_factory=NY_ingredients_factory()
+#     pizza_store=NYPizzaStore(ingredient_factory)
+#     pizza=pizza_store.orderPizza('veggie')
+#     assert pizza.box()=='Boxing BYveggie pizza'
 
+def test_store_able_to_get_ingredients_from_ingredient_factory():
+    ny_store=NYPizzaStore()
+    pizza=ny_store.orderPizza('cheese')
+
+    assert pizza.dough.name=='Thin crust dough'
+
+    chicago_store=ChicagoPizzaStore()
+    pizza=chicago_store.orderPizza('cheese')
+
+    assert pizza.dough.name=='Thick crust dough'
