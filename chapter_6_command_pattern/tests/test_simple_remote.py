328167df970c392d4219aeacc20485e0aecf2578
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-10 06:38:01 +0530

    added garage door

--- chapter_6_command_pattern/tests/test_simple_remote.py
+++ chapter_6_command_pattern/tests/test_simple_remote.py
@@ -1,4 +1,4 @@
-from simple_remote import simpleRemote,LightOnCommand,Light
+from simple_remote import simpleRemote,LightOnCommand,Light,GarageDoor,GaragerDoorOpenCommand
 
 def test_able_to_set_and_execute_light_on_command():
     light1=Light()
@@ -7,3 +7,17 @@ def test_able_to_set_and_execute_light_on_command():
     remote.set_command(command)
 
     assert remote.button_pressed()=='This light is on!'
+
+
+def test_able_to_set_and_execute_light_and_garage_door_on_command():
+    light1=Light()
+    command1=LightOnCommand(light1)
+    remote=simpleRemote()
+    remote.set_command(command1)
+
+    assert remote.button_pressed()=='This light is on!'
+    garage_door=GarageDoor()
+    command2=GaragerDoorOpenCommand(garage_door)
+    remote.set_command(command2)
+    
+    assert remote.button_pressed()=='This Garage Door is opened!'
