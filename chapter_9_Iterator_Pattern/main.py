14717508bb7420a2395d52abe508fafc6821fcff
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-18 06:27:59 +0530

    removed the need for wattiress modification

--- chapter_9_Iterator_Pattern/main.py
+++ chapter_9_Iterator_Pattern/main.py
@@ -6,7 +6,8 @@ if __name__=="__main__":
     panckae_menu=PancakeHouseMenu()
     diner_menu=DinerMenu()
     cafe_menu=CafeMenu()
+    menulist=[panckae_menu,diner_menu,cafe_menu]
 
-    waitress=Waitress(diner_menu,panckae_menu,cafe_menu)
+    waitress=Waitress(menulist)
 
     waitress.printAllMenu()
