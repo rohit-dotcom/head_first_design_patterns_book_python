705b2f0fdeb61776fa444aba440778d7b8d5af8b
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-18 05:57:15 +0530

    added cafe dinner menu

--- chapter_9_Iterator_Pattern/main.py
+++ chapter_9_Iterator_Pattern/main.py
@@ -1,11 +1,12 @@
 from waiteress import Waitress
-from menus import DinerMenu,PancakeHouseMenu
+from menus import DinerMenu,PancakeHouseMenu,CafeMenu
 
 
 if __name__=="__main__":
     panckae_menu=PancakeHouseMenu()
     diner_menu=DinerMenu()
+    cafe_menu=CafeMenu()
 
-    waitress=Waitress(diner_menu,panckae_menu)
+    waitress=Waitress(diner_menu,panckae_menu,cafe_menu)
 
     waitress.printAllMenu()
