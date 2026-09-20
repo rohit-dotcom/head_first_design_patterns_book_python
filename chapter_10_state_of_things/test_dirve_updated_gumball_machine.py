925863e289feb8a66f70934afa69100ebeef60bc
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-20 16:27:49 +0530

    bug fix for teh position for the dispense method

--- chapter_10_state_of_things/test_dirve_updated_gumball_machine.py
+++ chapter_10_state_of_things/test_dirve_updated_gumball_machine.py
@@ -2,10 +2,14 @@ from gumball_machine_with_states import Gumball_Machine
 
 
 if __name__=='__main__':
-    gumball_machine=Gumball_Machine(5)
+    gumball_machine=Gumball_Machine(2)
     gumball_machine.insertQuarter()
     gumball_machine.turnCrank()
     gumball_machine.ejectQuarter()
     gumball_machine.turnCrank()
     gumball_machine.insertQuarter()
     gumball_machine.turnCrank()
+    gumball_machine.insertQuarter()
+    gumball_machine.ejectQuarter()
+    gumball_machine.insertQuarter()
+    gumball_machine.ejectQuarter()
