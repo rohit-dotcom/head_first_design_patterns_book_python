02c3306dd8009e4d8d3175d52263478608e5aee9
Rohit Sharma <rohit.enggboy@gmail.com>
2026-09-20 17:18:26 +0530

    added winner state

--- chapter_10_state_of_things/test_dirve_updated_gumball_machine.py
+++ chapter_10_state_of_things/test_dirve_updated_gumball_machine.py
@@ -1,8 +1,9 @@
 from gumball_machine_with_states import Gumball_Machine
+import random
 
 
 if __name__=='__main__':
-    gumball_machine=Gumball_Machine(2)
+    gumball_machine=Gumball_Machine(100)
     gumball_machine.insertQuarter()
     gumball_machine.turnCrank()
     gumball_machine.ejectQuarter()
@@ -11,5 +12,7 @@ if __name__=='__main__':
     gumball_machine.turnCrank()
     gumball_machine.insertQuarter()
     gumball_machine.ejectQuarter()
-    gumball_machine.insertQuarter()
-    gumball_machine.ejectQuarter()
+    for i in range(20):
+
+        gumball_machine.insertQuarter()
+        gumball_machine.turnCrank()
